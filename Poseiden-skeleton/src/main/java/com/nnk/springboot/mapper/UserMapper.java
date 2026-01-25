ecfd12e905354bf67796a8ef2c3aaf0993331e9d
alcan <alcan@example.com>
2026-01-25 11:45:41 +0100

    mapper i

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+
+package com.nnk.springboot.mapper;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.dto.UserDto;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+
+    @Mapping(target = "password", ignore = true)
+    UserDto toDto(User entity);
+
+
+    User toEntity(UserDto dto);
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(@MappingTarget User entity, UserDto dto);
+}
