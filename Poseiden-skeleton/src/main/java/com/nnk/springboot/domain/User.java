3f8a1bf74671469be2d2688691d1180aaaee69ed
alcan <alcan@example.com>
2026-01-25 11:46:18 +0100

    user model

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -1,60 +1,36 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Table(name = "users")
+@Table( name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(exclude = "password")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Integer id;
-    @NotBlank(message = "Username is mandatory")
+
+    @Column(name = "username", nullable = false, length = 125)
     private String username;
-    @NotBlank(message = "Password is mandatory")
+
+    @Column(name = "password", nullable = false, length = 125)
     private String password;
-    @NotBlank(message = "FullName is mandatory")
+
+    @Column(name = "fullname", nullable = false, length = 125)
     private String fullname;
-    @NotBlank(message = "Role is mandatory")
+
+    @Column(name = "role", nullable = false, length = 125)
     private String role;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-}
