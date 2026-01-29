ecb8e8ccefa12867a9217cc57dad518c8b22da46
alcan <alcan@example.com>
2026-01-29 12:50:34 +0100

    userdetailservice

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/CustomUserDetailsService.java
@@ -2,19 +2,19 @@ package com.nnk.springboot.services;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
